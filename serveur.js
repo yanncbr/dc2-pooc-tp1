3fa7ebb51e5018f7426f4c59df8fa9e9db5b645a
yann <yann@MacBook-Pro-de-yann.local>
2018-09-18 16:21:30 +0200

    TP1 de POOC

---
+++ serveur.js
@@ -0,0 +1,15 @@
+const express = require('express')
+  const app = express()
+  const port = 3000
+  app.get('/', (request, response) => {
+    response.send('<h1> Bonjour</h1>')
+})
+  app.get('/hello/:name', (request, response) => {
+    response.send('Hello, '+request.params.name+'!');
+})
+  app.listen(port, (err) => {
+    if (err) {
+      return console.log('Erreur du serveur : ', err)
+    }
+    console.log('Le serveur écoute sur le port' + port + 'nRendez vous sur http://localhost:' + port
+  )})
